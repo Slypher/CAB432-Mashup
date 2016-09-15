f0ea5cb8c41e72624665122795ee11e224427398
Slypher <connorgill@live.com.au>
2016-09-15 17:35:59 +1000

    Updated formatting

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const favicon = require('serve-favicon');
 const morgan = require('morgan');
+const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 
-app.use(morgan('\x1b[36m:remote-addr :method :url \x1b[31m:status\x1b[37m  ---  :user-agent')); // log requests
-app.use(bodyParser.urlencoded());
+app.use(morgan('\x1b[90m:date[web] \x1b[92m:remote-addr \x1b[90m:method :url \x1b[36m:status\x1b[37m')); // log requests
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // get information from HTML forms
-//app.use(favicon(__dirname + '/public/images/favicon.ico')); // app icon
 app.use(express.static('public')); // make public folder available to app
+//app.use(favicon(__dirname + '/public/images/favicon.ico')); // app icon
 
 app.set('view engine', 'ejs'); // set up ejs for templating
  
 require('./routes.js')(app); // load routes
 
 app.listen(port); // launch
-console.log('Listening on port \x1b[36m' + port + '\x1b[37m');
+console.log('\x1b[90mListening on port \x1b[36m' + port + '\x1b[37m');
