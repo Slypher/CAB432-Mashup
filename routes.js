f482300dd166c5b968e99d9930a6e5929d3a3a2a
Slypher <connorgill@live.com.au>
2016-09-15 12:44:31 +1000

    Added one line break

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 'use strict';
+
 const https = require('https');
 
 module.exports = function (app) {
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 module.exports = function (req, res) {
     res.render('index.ejs', {});
 }
